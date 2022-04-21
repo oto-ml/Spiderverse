e902d2ff9a58936eac93e9402e71cf463aa6390e
oto-ml <rmunoz1803@alumno.ipn.mx>
2022-04-21 00:00:49 -0500

    Añadiendo atributos para test

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,6 +1,11 @@
-describe("Test Suite Dummy Description", () => {
-    test('Case 1 Dummy', () => {
-      const resultOfSomething = 1 + 2
-      expect(resultOfSomething).toBe(10);
+describe("Unit Tests for Spiderman class", () => {
+    test('1) Create a spiderman object', () => {
+      const AndrewGarfield = new Spiderman ("Amazing Spiderman", 31, "Andrew Garfield", 2, "Sony")
+      expect(AndrewGarfield.name).toBe("Amazing Spiderman");
+      expect(AndrewGarfield.age).toBe(31);
+      expect(AndrewGarfield.actor).toBe("Andrew Garfield");
+      expect(AndrewGarfield.movies).toBe(2);
+      expect(AndrewGarfield.studio).toBe("sony");
+
     });
   })
