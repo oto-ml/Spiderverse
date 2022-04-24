12ac97cda71e1ff88b38dfbd7899ae1536c7a212
oto-ml <rmunoz1803@alumno.ipn.mx>
2022-04-24 17:57:16 -0500

    arreglando prueba para resultado positivo

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,3 +1,5 @@
+const Spiderman = require (`./../app/spiderman`)
+
 describe("Unit Tests for Spiderman class", () => {
     test('1) Create a spiderman object', () => {
       const AndrewGarfield = new Spiderman ("Amazing Spiderman", 31, "Andrew Garfield", 2, "Sony")
@@ -5,7 +7,7 @@ describe("Unit Tests for Spiderman class", () => {
       expect(AndrewGarfield.age).toBe(31);
       expect(AndrewGarfield.actor).toBe("Andrew Garfield");
       expect(AndrewGarfield.movies).toBe(2);
-      expect(AndrewGarfield.studio).toBe("sony");
+      expect(AndrewGarfield.studio).toBe("Sony");
 
     });
   })
